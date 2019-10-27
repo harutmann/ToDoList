df703c66c2b356fa89fa96325e7a1869edcd3671
Rin <harutmann@gmail.com>
2019-10-27 19:22:30 +0300

    localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -26,9 +26,36 @@ list.addEventListener('click', function(evt) {
   }
 }, false);
 
+var storage = [];
+if (localStorage.getItem('storage') != undefined) {
+  storage = JSON.parse(localStorage.getItem('storage'));
+  console.log(`storage length: ${storage.length}`);
+  for (var i = 0; i < storage.length; i++) {
+    console.log(`storage id is : ${i}`)
+    let li = document.createElement('li');
+    let oldContent = document.createTextNode(storage[i]);
+    li.appendChild(oldContent);
+
+    document.getElementById("myList").appendChild(li);
+    let span = document.createElement("span");
+    let txt = document.createTextNode("❌");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (var c = 0; c < close.length; c++) {
+      close[c].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+}
+
 function newElement() {
   var li = document.createElement('li');
   var inputValue = document.getElementById('myInput').value;
+  storage.push(inputValue);
   var content = document.createTextNode(inputValue);
   li.appendChild(content);
 
@@ -59,5 +86,6 @@ button.addEventListener("click", function(evt) {
   evt.preventDefault();
 
   newElement();
+  localStorage.setItem('storage', JSON.stringify(storage));
 
 });
